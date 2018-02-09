13e51eb7614394ab65a621a369e39323d66650ed
TeckHuaNg <200348264@student.georgianc.on.ca>
2018-02-09 14:28:34 -0500

    initial commit

---
+++ comp2007pmMusicStore/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace comp2007pmMusicStore.Models
+{
+    public class Album
+    {
+        public string Title { get; set;}
+        public Genre genre { get; set; }
+        public string AlbumArtUrl { get; set; }
+        public double Price { get; set; }
+    }
+}
